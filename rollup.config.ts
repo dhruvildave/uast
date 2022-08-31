a68906027e4edc04b2d2ba6a1582c4da6fb1afd4
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2022-08-31 14:16:35 -0500

    Disable sourcemap in production

--- rollup.config.ts
+++ rollup.config.ts
@@ -37,7 +37,7 @@ function serve() {
 export default {
   input: 'src/main.ts',
   output: {
-    sourcemap: true,
+    sourcemap: !production,
     format: 'iife',
     name: 'app',
     file: 'public/build/bundle.js',
