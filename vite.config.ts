3f0e3844002670a009b39f398204f6bc7c56a887
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2022-10-23 20:53:41 -0500

    Add build options to vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      treeshake: true,
+      strictDeprecations: true,
+    },
+  },
 });
