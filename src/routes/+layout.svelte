e6b752a3faf42d1f9b899a72df8792510e608ee6
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2024-03-06 02:15:31 -0600

    Versioning

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { beforeNavigate } from "$app/navigation";
+  import { updated } from "$app/stores";
+
+  beforeNavigate(({ willUnload, to }) => {
+    if ($updated && !willUnload && to?.url) {
+      location.href = to.url.href;
+    }
+  });
+</script>
+
+<slot />
