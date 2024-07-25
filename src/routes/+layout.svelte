7f10b6ce2f01e31f2ab2e3cc3316f52dfcfa5dc6
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2024-07-25 15:21:21 -0500

    Add vercel speed insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import { beforeNavigate } from "$app/navigation";
   import { updated } from "$app/stores";
+  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
 
   beforeNavigate(({ willUnload, to }) => {
     if ($updated && !willUnload && to?.url) {
       location.href = to.url.href;
     }
   });
+
+  injectSpeedInsights();
 </script>
 
 <slot />
