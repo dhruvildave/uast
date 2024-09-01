34b7067f853fa17014ffe7a19fbb50b20df1b196
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2024-09-01 18:57:28 -0500

    Remove @vercel/speed-insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
   import { beforeNavigate } from "$app/navigation";
   import { updated } from "$app/stores";
-  import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
 
   beforeNavigate(({ willUnload, to }) => {
     if ($updated && !willUnload && to?.url) {
       location.href = to.url.href;
     }
   });
-
-  injectSpeedInsights();
 </script>
 
 <slot />
